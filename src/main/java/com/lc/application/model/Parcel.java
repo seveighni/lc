61504c4e5f8901a8655fd07d870cad048749f98d
mptw <mp.todorowa@gmail.com>
2024-02-04 20:22:34 +0200

    adding base entity for Parcel

---
+++ src/main/java/com/lc/application/model/Parcel.java
@@ -0,0 +1,57 @@
+package com.lc.application.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "parcels")
+public class Parcel {
+
+	// TODO
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = true)
+	@JoinColumn(name = "sender_id", nullable = false)
+	private Customer sender;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = true)
+	@JoinColumn(name = "receiver_id", nullable = false)
+	private Customer receiver;
+
+	private String address;
+
+	private double weight;
+
+	// @Transient ?
+	// private Rates rate;
+
+	// @Formula
+	private BigDecimal price;
+
+	// enum ?
+	private String status;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = true)
+	@JoinColumn(name = "employee_id", nullable = true)
+	private Employee registeredBy;
+
+}
