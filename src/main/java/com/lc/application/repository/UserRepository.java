5ca22c1c4447e79e0945f086666da397e21aee9a
mptw <mp.todorowa@gmail.com>
2024-02-05 22:07:28 +0200

    added users & users waiting approval to be filtered when searched, fixed paging

--- src/main/java/com/lc/application/repository/UserRepository.java
+++ src/main/java/com/lc/application/repository/UserRepository.java
@@ -4,10 +4,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.lc.application.model.Role;
 import com.lc.application.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	User findByEmail(String email);
 
+	Page<User> findByRoles(Role roles, Pageable pageable);
+
 	Page<User> findByEmailContainingIgnoreCase(String email, Pageable pageable);
+
+	Page<User> findByRolesAndEmailContainingIgnoreCase(Role roles, String email, Pageable pageable);
 }
